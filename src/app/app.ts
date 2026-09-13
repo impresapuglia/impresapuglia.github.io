4cf36398cbeef8e893ed5d30a1a992a41a831890
Rhaastrake <garofalomichele2001@gmail.com>
2026-09-13 11:34:47 +0200

    First commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('impresapuglia');
+  protected readonly title = signal('Impresa Puglia');
 }
